0c5f67442436c18b2078567bfde2b3ad1b90946a
joey abouharb <joeyabouharb@localhost.localdomain>
2019-10-24 17:59:27 +1100

    front-end is working...

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom"
 import App from './components/app'
-
+import React from 'react'
 
 ReactDOM.render(
     <App />,
-    document.getElementById('root')
+    document.getElementById('app')
 );
