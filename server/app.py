4762e3175eca33e47d8e0fd3c16a6674fbe67cec
Staff GVA <GVA@Sach-Australia.local>
2019-10-25 04:02:38 +1100

    Got the new blog post route working

--- server/app.py
+++ server/app.py
@@ -57,6 +57,25 @@ def display_blog_posts():
     result = posts_schema.dump(blog)
     return jsonify(result)
 
+@app.route('/blog/<int:post_id>', methods=['GET'])
+def display_single_post(post_id: int):
+    post = Post.query.filter_by(post_id=post_id).first()
+    if post:
+        result = post_schema.dump(post)
+        return jsonify(result)
+    else:
+        return jsonify(message="Post does not exist"), 404
+
+@app.route('/new-post', methods=['POST'])
+def make_new_blog_post():
+    """Accepts form data and creates new database record in blog-post table"""
+    title = request.form['title']
+    content = request.form['content']
+    new_post = Post(title=title, content=content)
+    db.session.add(new_post)
+    db.session.commit()
+    return jsonify(message="New blog post created"), 201
+
 @app.route('/register', methods=['POST'])
 def register():
     print(request.args)
