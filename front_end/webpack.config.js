d6b12e05ab5134a74af629d087534ac1f73bbc28
joey abouharb <joeyabouharb@localhost.localdomain>
2019-10-27 13:43:13 +1100

    used jsx instead of createElement. does it look better?

--- front_end/webpack.config.js
+++ front_end/webpack.config.js
@@ -1,5 +1,5 @@
-let path = require('path');
-let HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.jsx',
